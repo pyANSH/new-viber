bd4047163493774ec2caa2d3c8ccc23e8985d48e
PRANTOSH <prantoshv@gmail.com>
2021-01-26 21:47:22 +0530

    added select in forms and its price logic JS

--- form.php
+++ form.php
@@ -3,19 +3,19 @@ if (isset($_POST['submit'])) {
     $name=$_POST['name'];
     $phone=$_POST['phone'];
     $pickup=$_POST['pickup'];
-$return=$_POST['return'];
-$pickAddress=$_POST['pickAddress'];
-$dropAddress=$_POST['dropAddress'];
+    $return=$_POST['return'];   
+    $pickAddress=$_POST['pickAddress'];
+    $dropAddress=$_POST['dropAddress'];
     $to='prantoshv@gmail.com';
     $subject='Form Submission';
-    $message="Name: " .$name."\n"."Name: " .$phone."\n"."Name: " .$pickup."\n"."Name: " .$return."\n"."Name: " .$pickAddress."\n"."Name: " .$dropAddress;
-    $header="From".$email;
+    $message="Name: " . $name ."\n\n" . "Name: " . $phone . "\n" . "Name: " . $pickup ."\n" . "Name: " . $return . "\n" . "Name: " . $pickAddress . "\n" . "Name: " . $dropAddress;
+    $headers="From" . $name;
 
-    if (mail($to,$subject,$message,$header)) {
+    if (mail($to,$subject,$message,$headers)) {
         echo "<h1>Sent Successfully</h1>";
     }
     else{
         echo  "Error, Something went wrong";
     }
-};
+}
 ?>
