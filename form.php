d67412a46d937d1c2352a68bd781b7335971d783
PRANTOSH <prantoshv@gmail.com>
2021-01-30 14:38:19 +0530

    Creating new form and fixed bug

--- form.php
+++ form.php
@@ -2,13 +2,14 @@
 if (isset($_POST['submit'])) {
     $name=$_POST['name'];
     $phone=$_POST['phone'];
-    $pickup=$_POST['pickup'];
-    $return=$_POST['return'];   
-    $pickAddress=$_POST['pickAddress'];
-    $dropAddress=$_POST['dropAddress'];
+    $pickup=$_POST['pickupDate'];
+    $return=$_POST['DropDate'];
+    $email=$_POST['Email'];   
+    $pickAddress=$_POST['Pickup'];
+    $dropAddress=$_POST['Drop'];
     $to='prantoshv@gmail.com';
     $subject='Form Submission';
-    $message="Name: " . $name ."\n" . "phone: " . $phone . "\n" . "pickup date: " . $pickup ."\n" . "return date: " . $return . "\n" . "Pickup address:Test1 " . $pickAddress . "\n" . "drop address: " . $dropAddress;
+    $message="Name: " . $name ."\n" . "phone: " . $phone . "\n" ."Email".$email. "pickup date: " . $pickup ."\n" . "return date: " . $return . "\n" . "Pickup address:Test1 " . $pickAddress . "\n" . "drop address: " . $dropAddress;
     $headers="From" . $name;
     $headers='MIME-Version: 1.0' . "\r\n";
     $headers='Content-type : text/html; charset=iso-8859-1' . "\r\n";
